edf1358965ee5b5eccd240e1dd27d67d829e9157
zhengted <zhengted@163.com>
2020-12-27 16:21:54 +0800

    LookForYou chapter2

--- db/mysql/conn.go
+++ db/mysql/conn.go
@@ -10,11 +10,11 @@ import (
 var db *sql.DB
 
 func init() {
-	db, _ = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/fileserver?charset=utf8")
+	db, _ = sql.Open("mysql", "root:root@tcp(127.0.0.1:13306)/fileserver?charset=utf8")
 	db.SetMaxOpenConns(1000)
 	err := db.Ping()
 	if err != nil {
-		fmt.Println("Failed to connext to mysql, err:" + err.Error())
+		fmt.Println("Failed to connect to mysql, err:" + err.Error())
 		os.Exit(1)
 	}
 }
