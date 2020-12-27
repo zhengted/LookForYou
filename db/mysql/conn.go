930fa0a0f3fc34578afdaafb141680251452fc6a
zhengted <zhengted@163.com>
2020-12-27 16:25:14 +0800

    Connect sql with password

--- db/mysql/conn.go
+++ db/mysql/conn.go
@@ -10,7 +10,7 @@ import (
 var db *sql.DB
 
 func init() {
-	db, _ = sql.Open("mysql", "root:root@tcp(127.0.0.1:13306)/fileserver?charset=utf8")
+	db, _ = sql.Open("mysql", "root:123456@tcp(127.0.0.1:13306)/fileserver?charset=utf8")
 	db.SetMaxOpenConns(1000)
 	err := db.Ping()
 	if err != nil {
