298eb8d694d863881c22d72c2e28b79ee95ee8b8
zhengted <zhengted@163.com>
2020-12-30 17:55:42 +0800

    修改redis连接端口

--- db/redisConn/conn.go
+++ db/redisConn/conn.go
@@ -7,7 +7,7 @@ import (
 func Conn() *redis.Pool {
 	pool := &redis.Pool{
 		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", "127.0.0.1:16379")
+			return redis.Dial("tcp", "0.0.0.0:6379")
 		},
 		MaxActive: 100,
 	}
