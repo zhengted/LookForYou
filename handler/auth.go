51321c9642977509f84e62ed5a7eb32f029c82f6
zhengted <zhengted@163.com>
2021-01-02 18:55:32 +0800

    分块上传拦截器收到请求打印

--- handler/auth.go
+++ handler/auth.go
@@ -8,12 +8,12 @@ import (
 func HTTPInterceptor(h http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println("拦截器Inceptor工作")
 			r.ParseForm()
 			username := r.Form.Get("username")
 			token := r.Form.Get("token")
 
 			if len(username) < 3 || !IsTokenValid(token, username) {
+				fmt.Println("拦截器Inceptor工作")
 				w.WriteHeader(http.StatusForbidden)
 				return
 			}
