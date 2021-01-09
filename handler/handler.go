175dd5e2c2d940cd9c829e65b7c1b313144e67fb
zhengted <zhengted@163.com>
2021-01-09 23:20:34 +0800

    修改文件存儲路徑

--- handler/handler.go
+++ handler/handler.go
@@ -34,35 +34,35 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		// 接受文件流及存储到本地目录
 		file, head, err := r.FormFile("file")
 		if err != nil {
-			fmt.Printf("failed to get data, err:%s", err.Error())
+			fmt.Printf("Failed to get data, err:%s\n", err.Error())
 			return
 		}
 		defer file.Close()
 
-		// TODO：
-		// 这立的优化空间 Location可以改为路径 + 文件的sha1值
-		// 因为文件的sha1是不会变的
+		tmpPath := cfg.TempLocalRootDir + head.Filename
 		fileMeta := meta.FileMeta{
 			FileName: head.Filename,
-			Location: "/home/ubuntu/FileStoreLoc/" + head.Filename,
+			Location: tmpPath,
 			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
 		}
 
 		newFile, err := os.Create(fileMeta.Location)
 		if err != nil {
-			fmt.Printf("Failed to create file, err%s\n", err.Error())
+			fmt.Printf("Failed to create file, err:%s\n", err.Error())
 			return
 		}
 		defer newFile.Close()
 
 		fileMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
-			fmt.Printf("Failed to save data into file,err %s\n", err.Error())
+			fmt.Printf("Failed to save data into file, err:%s\n", err.Error())
 			return
 		}
 
 		newFile.Seek(0, 0)
 		fileMeta.FileSha1 = util.FileSha1(newFile)
+
+		newFile.Seek(0, 0) // 游标重新回到文件头部
 		mergePath := cfg.MergeLocalRootDir + fileMeta.FileSha1
 		if cfg.CurrentStoreType == cmn.StoreCeph {
 			// 文件写入Ceph存储
