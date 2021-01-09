792b3dbbe6a189828b302f56a74b2d00170f5bd4
zhengted <zhengted@163.com>
2021-01-09 23:17:52 +0800

    修改文件存儲路徑

--- config/store.go
+++ config/store.go
@@ -4,11 +4,11 @@ import cmn "LookForYou/common"
 
 const (
 	// TempLocalRootDir : 本地临时存储地址的路径
-	TempLocalRootDir = "/home/ubuntu/FileStoreData/tmp"
+	TempLocalRootDir = "/home/ubuntu/FileStoreData/tmp/"
 	// MergeLocalRootDir : 本地存储地址的路径(包含普通上传及分块上传)
-	MergeLocalRootDir = "/home/ubuntu/FileStoreData/merge"
+	MergeLocalRootDir = "/home/ubuntu/FileStoreData/merge/"
 	// ChunckLocalRootDir : 分块存储地址的路径
-	ChunckLocalRootDir = "/home/ubuntu/FileStoreData/chunks"
+	ChunckLocalRootDir = "/home/ubuntu/FileStoreData/chunks/"
 	// CurrentStoreType : 设置当前文件的存储类型
 	CurrentStoreType = cmn.StoreOSS
 )
