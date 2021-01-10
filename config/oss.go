80fb4dee821ef2d444abb78450620139067592e3
zhengted <zhengted@163.com>
2021-01-10 17:59:33 +0800

    http的相关处理改为使用Gin框架

--- config/oss.go
+++ config/oss.go
@@ -3,6 +3,6 @@ package config
 const (
 	OSSBucket          = "ted-filestore"
 	OSSEndPoint        = "oss-cn-shanghai.aliyuncs.com"
-	OSSAccesskeyID     = "LTAI4GBdUMriWoVqa1d5LeNF"
-	OSSAccesskeySecret = "YRkkiFo0QIoLB06rEv9ZpfxeAeasPZ"
+	OSSAccesskeyID     = "YouCanNotSee"
+	OSSAccesskeySecret = "YouCanNotSee"
 )
